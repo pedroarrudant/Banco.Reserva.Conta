b784d9a8808e7e473c24a45f19c56df60d0233a1
Pedro Arruda <pedro.arruda@outlook.com.br>
2021-06-30 22:51:48 -0300

    Ajustes no build da aplicacao

---
+++ src/main/java/Banco/Reserva/Conta/Controller/ContaController.java
@@ -0,0 +1,20 @@
+package Banco.Reserva.Conta.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("conta")
+public class ContaController {
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+
+        return String.format("Hello %s", name);
+    }
+}
---
+++ src/main/java/Banco/Reserva/Conta/Startup.java
@@ -0,0 +1,12 @@
+package Banco.Reserva.Conta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+        //SpringApplication.run(ContaController.class, args);
+    }
+}

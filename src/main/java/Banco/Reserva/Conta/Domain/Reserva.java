cae2626510a78be74f20dcca4fec086e5670cdf5
Pedro <pedro.arruda@outlook.com.br>
2021-08-15 02:17:37 -0300

    Ajustes no build do Docker

--- src/main/java/Banco/Reserva/Conta/Domain/Reserva.java
+++ src/main/java/Banco/Reserva/Conta/Domain/Reserva.java
@@ -4,9 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import nonapi.io.github.classgraph.json.Id;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
--- src/main/java/Banco/Reserva/Conta/Domain/Reservas.java
+++ src/main/java/Banco/Reserva/Conta/Domain/Reservas.java
@@ -2,12 +2,9 @@ package Banco.Reserva.Conta.Domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import nonapi.io.github.classgraph.json.Id;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import org.springframework.data.annotation.Id;
 import java.util.ArrayList;
 import java.util.List;
 
